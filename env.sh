795aebd47b78664507a1d144b853658471d4d96e
Thomas Brettin <brettin@anl.gov>
2023-12-20 19:37:04 +0000

    aurora env settings

---
+++ env.sh
@@ -0,0 +1,22 @@
+# aurora_examples
+
+
+# qsub -l select=2 -l walltime=30:00 -A CSC249ADOA01_CNDA -q EarlyAppAccess -I
+
+
+export HTTP_PROXY=http://proxy.alcf.anl.gov:3128
+export HTTPS_PROXY=http://proxy.alcf.anl.gov:3128
+export http_proxy=http://proxy.alcf.anl.gov:3128
+export https_proxy=http://proxy.alcf.anl.gov:3128
+git config --global http.proxy http://proxy.alcf.anl.gov:3128
+
+
+module use /soft/modulefiles
+module load frameworks
+
+
+export PYTHONPATH=$PYTHONPATH:.
+
+
+# mpiexec --ppn 12 --np 24 gpu_tile_compact.sh python ./aurora_train.py --infile ./infiles.24
+
